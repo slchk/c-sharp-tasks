525d9f4ad31e6dcf9c7031d42a3be08de83ec0f5
Elena Sluchak <lena.sluchak@gmail.com>
2024-03-25 21:52:42 +0100

    My first "Hello, user" app

---
+++ task-0/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string user = Console.ReadLine();
+            Console.WriteLine("Hello, " + user);
+        }
+    }
+}
