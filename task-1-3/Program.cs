846c61c2b5723fb99fb51c61a39dc6e2bb48c7a3
Elena Sluchak <lena.sluchak@gmail.com>
2024-03-26 22:28:47 +0100

    Homework number 1: Variables and operations

---
+++ task-1-3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a weather-related word in Russian");
+            string sign = Console.ReadLine();
+            switch (sign) 
+            {
+                case "тепло":
+                    Console.WriteLine("warm");
+                    break;
+                case "холодно":
+                    Console.WriteLine("cold");
+                    break;
+                case "погода":
+                    Console.WriteLine("weather");
+                    break;
+                case "жарко":
+                    Console.WriteLine("hot");
+                    break;
+                case "дождь":
+                    Console.WriteLine("rain");
+                    break;
+                case "гром":
+                    Console.WriteLine("thunder");
+                    break;
+                case "снег":
+                    Console.WriteLine("snow");
+                    break;
+                case "солнечно":
+                    Console.WriteLine("sunny");
+                    break;
+                case "ветрено":
+                    Console.WriteLine("windy");
+                    break;
+                case "облачно":
+                    Console.WriteLine("cloudy");
+                    break;
+                default:
+                    Console.WriteLine("I know only 10 weather-related words. You entered a word I don't know. Please try again.");
+                    break;
+            }
+        }
+    }
+}
---
+++ task-1-4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("The first number is even");
+            }
+            else
+            {
+                Console.WriteLine("The first number is odd");
+            }
+
+            int number2 = 6;
+            string message = (number2 % 2 == 0) ? "even" : "odd";
+            Console.WriteLine($"The second number is {message}");
+        }
+    }
+}
